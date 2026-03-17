36a368458f5975a2540009a8d389650bcf10c238
LEE JUNE <mars102aq@gmail.com>
2026-03-17 19:29:27 +0900

    chore: SDKMAN! 설치 시 zsh를 사용하도록 변경

--- bootstrap.sh
+++ bootstrap.sh
@@ -98,7 +98,7 @@ ensure_has_sdkman() {
     printf '☕ Checking for SDKMAN! ... '
     [[ -d "$HOME/.sdkman" ]] && printf 'found\n' || {
         printf 'installing\n'
-        curl -s "https://get.sdkman.io" | bash
+        zsh <(curl -s "https://get.sdkman.io")
     }
 }
 
